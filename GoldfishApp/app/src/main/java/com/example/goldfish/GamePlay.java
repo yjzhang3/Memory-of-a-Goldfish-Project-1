819fe35a2bb8d39f333328ec1a8f2854d6202e45
myliang47 <56775136+myliang47@users.noreply.github.com>
2020-04-28 08:57:04 -1000

    Added in comments for readability
    Only added in comments for the code I wrote

--- GoldfishApp/app/src/main/java/com/example/goldfish/GamePlay.java
+++ GoldfishApp/app/src/main/java/com/example/goldfish/GamePlay.java
@@ -24,15 +24,18 @@ import java.util.List;
 
 public class GamePlay extends AppCompatActivity implements PauseGameFragment.PauseDialogListener {
 
+    // Timer counts down starting from 5 minutes, or 300 seconds
     private static final long START_TIME_IN_MILLIS = 300000;
     private TextView countDownText;
     private Button countDownButton;
 
+    // Timer variables
     private CountDownTimer countDownTimer;
     private long timeLeftMilliseconds = START_TIME_IN_MILLIS;
     private String timeUsed;
     private boolean timerRunning;
 
+    // Game play variables
     int numCards = 16;
     int numMatchedCards = 0, firstCardPosition, moveCount = 0;
     final List<Integer> cardImages = new ArrayList<>();
@@ -43,21 +46,17 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
     boolean firstClick = false;
     Handler handler;
 
-    // DECLARE DIFFICULTY VARIABLE HERE
+    // Difficulty settings buttons
     private RadioButton radioHard;
     private RadioButton radioMedium;
     private RadioButton radioEasy;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
+        // Pause button
         Button fab = findViewById(R.id.pause);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,9 +65,6 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
                 stopTimer();
             }
         });
-
-        //adjustSettings();
-        //pass in difficulty from settings & change number of cards based on difficulty
 
         countDownText = findViewById(R.id.Start_countdown);
         countDownButton = findViewById(R.id.button_first_startgame);
@@ -79,16 +75,21 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
 
             }
         });
-
-        adjustSettings(); // pass in difficulty from settings & change number of cards based on difficulty
+        
+        // pass in difficulty from settings & change number of cards based on difficulty
+        adjustSettings(); 
         loadImagesAndCards();
 
+        // loads gridview of cards
         cardDisplayView = findViewById(R.id.grid_view);
+        // uses adapter to fill out each grid cell with an image initially
         cardImageAdapter = new CardImageAdapter(this, imageID, numCards);
         cardDisplayView.setAdapter( cardImageAdapter);
+        // shuffle the images on the gameboard
         cardImageAdapter.shuffle();
         cardImageAdapter.notifyDataSetChanged();
 
+        // allows action to be taken when player clicks on a card
         playGame();
 
     }
@@ -99,14 +100,18 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
         SharedPreferences sharedPreferences = getSharedPreferences("diffData", MODE_PRIVATE);
         int value = sharedPreferences.getInt("checkedRadioButtonId",0); // value has the value of that in sharedPrefs
 
-        if (value==R.id.radioHard) { // hard, shows 4 x 4 card array on the game play screen
+        // hard, shows 4 x 4 card array on the game play screen
+        if (value==R.id.radioHard) { 
             numCards = 16;
         }
-        else if (value==R.id.radioMedium) {  // medium, 3 x 4 card array
+        
+        // medium, 3 x 4 card array
+        else if (value==R.id.radioMedium) {  
             numCards = 12;
         }
-
-        else { // easy, 2 x 4 card array
+        
+        // easy, 2 x 4 card array
+        else { 
             numCards = 8;
         }
     }
@@ -168,6 +173,7 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
 
     }
 
+    // DELETE?
 //    public void showNoticeDialog() {
 //        DialogFragment dialog = new PauseGameFragment();
 //        dialog.show(getSupportFragmentManager(),"PauseMessageDialog");
@@ -249,6 +255,8 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
+                
+                // Loading the images from the drawable folder
                 cardImages.add(R.drawable.pair1);
                 cardImages.add(R.drawable.pair2);
                 cardImages.add(R.drawable.pair3);
@@ -258,6 +266,7 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
                 cardImages.add(R.drawable.pair7);
                 cardImages.add(R.drawable.pair8);
 
+                // assigns each cardImage index to two indices so there will be pairs of cards
                 for( int i = 0; i < (numCards / 2); i++) {
                     imageID[i] = i;
                     imageID[ i + (numCards / 2)] = i;
@@ -268,37 +277,49 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
         mythread.start();
     }
 
+    // This function allows the app to take action whenever the player clicks a card
     public void playGame() {
+        
         Runnable runnable = new Runnable() {
+            
             @Override
             public void run() {
+                
                 cardDisplayView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                        
+                        // shows the card face once the player clicks the card
                         imageView = (ImageView) view;
                         imageView.setImageResource(cardImages.get( imageID[ position]));
 
+                        // keeping track of clicks two at a time
                         if (!firstClick) {
                             firstClick = true;
                             firstCardPosition = position;
                             firstCardView = imageView;
-                            moveCount ++;
-
-                        } else if (position != firstCardPosition) {
+                        } 
+                        
+                        // second click only works if a different card is clicked
+                        else if (position != firstCardPosition) {
                             firstClick = false;
-                            secondCardView = imageView;
-                            moveCount ++;
-                            checkMatch(position, firstCardPosition, firstCardView, secondCardView);
+                            secondCardView = imageView; 
+                            // checking if the cards match
+                            checkMatch(position, firstCardPosition, firstCardView, secondCardView); 
                         }
                     }
                 });
             }
         };
+        
+        // running this function in a different thread from the main one to prevent possible crashing
         Thread mythread = new Thread(runnable);
         mythread.start();
     }
 
+    // After two cards are clicked, this function checks if they match
+    // If two cards match, they disappear from the game board
+    // If they don't match, the back sides of the cards are shown again
     public void checkMatch(final int pos1, final int pos2, final ImageView firstCardView, final ImageView secondCardView) {
 
         handler = new Handler();
@@ -306,22 +327,27 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                if( imageID[ pos1] == imageID[ pos2]) {
+                
+                // clearing the 2 cards from the board
+                if( imageID[ pos1] == imageID[ pos2]) { 
+                    
                     numMatchedCards += 2;
                     firstCardView.setVisibility( View.INVISIBLE);
                     firstCardView.setClickable( false);
                     secondCardView.setVisibility( View.INVISIBLE);
                     secondCardView.setClickable( false);
 
+                    // timer stops once all the pairs of cards are matched 
                     if ( numMatchedCards == numCards) {
 
                         stopTimer();
                         FragmentManager manager = getSupportFragmentManager();
                         WinLoseFragment dialog = new WinLoseFragment();
                         dialog.show(manager, "MessageDialog");
-
                     }
                 }
+                
+                //shows card back if two cards don't match
                 else {
                     firstCardView.setImageResource( R.drawable.back);
                     secondCardView.setImageResource( R.drawable.back);
@@ -329,6 +355,8 @@ public class GamePlay extends AppCompatActivity implements PauseGameFragment.Pau
             }
         };
 
+        // Delay of 0.6 second after the player clicks the second card
+        // before the card faces are hidden or cleared  
         handler.postDelayed( runnable, 600);
     }
 }
