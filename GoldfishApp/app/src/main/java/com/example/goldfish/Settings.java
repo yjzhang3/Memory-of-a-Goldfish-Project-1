dc1bcee72b4f4d3824255d47b34fc6c8ef964eea
Lesbeth Roque <44933708+lsroque@users.noreply.github.com>
2020-04-28 16:28:07 -0400

    Added comments for readability

--- GoldfishApp/app/src/main/java/com/example/goldfish/Settings.java
+++ GoldfishApp/app/src/main/java/com/example/goldfish/Settings.java
@@ -13,6 +13,7 @@ public class Settings extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
 
+    // Used to return back to the previous activity
     getSupportActionBar().setTitle("MainMenu");
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -26,23 +27,23 @@ public class Settings extends AppCompatActivity {
 //
 //        }
 //    });
-
+    
+    // Button leads to another activity where the user can choose their preferred level
     Button difficultyLevel = (Button)findViewById(R.id.difficultyLevel);
         difficultyLevel.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent startIntent = new Intent(getApplicationContext(), DifficultyLevel.class);
-            // show how to pass information to another activity
             startActivity(startIntent);
         }
     });
-
+    
+    // Button leads to another activity where the user can choose to turn sounds on or off
     Button soundSettings = (Button)findViewById(R.id.settings);
         soundSettings.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent startIntent = new Intent(getApplicationContext(),SoundSettings.class);
-            // show how to pass information to another activity
             startActivity(startIntent);
         }
     });
