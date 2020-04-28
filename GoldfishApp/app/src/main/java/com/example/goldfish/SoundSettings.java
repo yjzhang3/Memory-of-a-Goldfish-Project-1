ece9cb0e64fdef684bc9771cc0c3dd393335ac72
Lesbeth Roque <44933708+lsroque@users.noreply.github.com>
2020-04-28 16:35:56 -0400

    Added comments for readability

--- GoldfishApp/app/src/main/java/com/example/goldfish/SoundSettings.java
+++ GoldfishApp/app/src/main/java/com/example/goldfish/SoundSettings.java
@@ -19,7 +19,8 @@ public class SoundSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_settings);
-
+        
+        // Used to return back to previous activity
         getSupportActionBar().setTitle("Settings");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -30,32 +31,38 @@ public class SoundSettings extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked){
-                    //the toggle is enabled, music plays!
+                    // The toggle is enabled, music plays!
                     if (MainActivityHomeMenu.mediaPlayer == null) {
                         MainActivityHomeMenu.mediaPlayer = MediaPlayer.create(SoundSettings.this, R.raw.bensoundsummer);
+                        // On default, the create the mediaplayer
                     }
                     MainActivityHomeMenu.mediaPlayer.setLooping(true);
                     MainActivityHomeMenu.mediaPlayer.start();
                 }
-                else{
-                    //the toggle is disabled, music stops
+                else {
+                    // The toggle is disabled, music stops
                     MainActivityHomeMenu.mediaPlayer.stop();
                     MainActivityHomeMenu.mediaPlayer.release();
                     MainActivityHomeMenu.mediaPlayer = null;
+                    // Release to stop holding on to mediaPlayer when this activity is not interacting with the user
                 }
                 saveData(isChecked);
             }
         });
 
     }
+    // Data is saved in shared preferences as a boolean value. This value is used to check if music should be playing 
+    // on app startup
     public void saveData(boolean isOnOff){
         SharedPreferences sharedPreferences = getSharedPreferences("musicToggle", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean("musicToggle",isOnOff);
         editor.apply();
+        // Toast to indicate the changes were saved
         Toast.makeText(this,"Game Setting saved!",Toast.LENGTH_SHORT).show();
     }
-
+    
+    // Data is loaded from last checked setting 
     public boolean loadData(){
         SharedPreferences sharedPreferences = getSharedPreferences("musicToggle", MODE_PRIVATE);
         return sharedPreferences.getBoolean("musicToggle", true);
